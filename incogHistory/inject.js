8c1d01e499b461aa8ab8ea1314d4a622213caf04
pupudu <pubudu_pincode@live.com>
2015-07-25 16:59:33 +0530

    clear storage

--- incogHistory/inject.js
+++ incogHistory/inject.js
@@ -1,1 +1,12 @@
 var href = window.location.href;
+/*document.addEventListener('DOMContentLoaded', function() {
+	console.log("Inside doc evt listner");
+	chrome.tabs.getSelected(tab.id, function(tab) {
+		console.log(tab.url);
+	});
+},false);*/
+
+chrome.storage.sync.clear(function(){});
+
+
+
